bccbf6744fdf1b9ae02cf8fc0c614710a55064e9
Neeraj <r1008394@student.thomasmore.be>
2026-04-02 13:25:27 +0200

    Python exercises day 3

--- 30DaysOfPython_Day2/variables.py
+++ 30DaysOfPython_Day2/variables.py
@@ -1,5 +1,37 @@
-# Day 2: 30 Days of python programming
+# Day 2: 30 Days of python programming 
 first_name = 'Pablo'
 last_name = 'Eduardo'
 full_name = first_name + ' ' + last_name
-print(full_name)
+# print(full_name) 
+# print(first_name + last_name)
+country = 'India'
+city = 'Thiruvananthapuram'
+age = 27
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+gender, school = 'male', 'KV'
+
+
+print(type(first_name)); 
+print(len(first_name))
+print(len(first_name) == len(last_name))
+num_one = 5
+num_two = 4
+total = num_one +  num_two
+diff = num_one - num_two
+product = num_two * num_one 
+division = num_one/num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+area_of_circle = 3.14 * 30**2
+circum_of_circle = 2 * 3.14 * 30
+# radius = float(input('What is the radius?'))
+# area = 3.1415926535 * radius**2
+# print(area)
+
+# print('True == True: ', True == True)
+# print('True == False: ', True == False)
+# print('False == False:', False == False)
---
+++ 30DaysOfPython_Day3/03_OperatorsExercises.py
@@ -0,0 +1,80 @@
+import math
+
+#Day3OfPython exercises
+# age = 26
+# my_height = 1.6
+# complex_number = 3 + 4j
+
+# Calculate area of a triangle
+# base = float(input('Enter the base of your triangle: '))
+# height = float(input('Enter the height of your triangle: '))
+# area = (base / 2) * height
+# print(area)
+
+# Calculate perimeter of triangle
+# side_a  = int(input('Enter side a'))
+# side_b  = int(input('Enter side b'))
+# side_c = int(input('Enter side c'))
+# perimeter = side_a + side_b + side_c 
+# print(perimeter)
+
+# 8th and 9th
+# x1,y1,x2,y2 = 2,2,6,10
+# slope =  (y2 - y1) / (x2 - x1)
+# Eucli_distance = math.sqrt(((x2 - x1)**2 + (y2 - y1)**2)) 
+
+# 11th
+# x = int(input('Enter a number'))
+# y = x**2 + 6 * x + 9
+# print(y)
+
+#12 
+# python_length = len('python')
+# dragon_length = len('dragon')
+# print(python_length > dragon_length)
+
+#13
+# print('on' in 'python' and 'on' in 'dragon')
+
+#14
+# print('jargon' in "I hope this course is not full of jargon")
+
+#15
+# print('on' not in 'python' and 'on' not in 'dragon')
+
+#16
+# word_python = str(float(len('python')))  
+
+#17
+# number = int(input('Enter a number: '))
+# print(number % 2 == 0)
+
+#18
+# print(7 // 3 == int(2.7))
+
+#19
+# print(type('10') == type(10)) 
+# print(type('10') is type(10))
+# print(type('10') is not type(10))
+
+#20
+# print(int(float('9.8')) == 10)
+
+#21
+# hours = float(input('Enter hours: '))
+# rate_per_hour = float(input('Enter rate per hour: '))
+# rent_per_week = hours * rate_per_hour
+# print(f'Your weekly earning is {rent_per_week}')
+
+#22
+# num_of_years = float(input('Enter number of years: '))
+# num_of_seconds = num_of_years * 365 * 24 * 60 * 60
+# print(f'You have lived for a total of {num_of_seconds} seconds.')
+
+#23
+# print(1, 1**0, 1**1, 1**2, 1**3)
+# print(2, 2**0, 2**1, 2**2, 2**3)
+# print(3, 3**0, 3**1, 3**2, 3**3)
+# print(4, 4**0, 4**1, 4**2, 4**3)
+# print(5, 5**0, 5**1, 5**2, 5**3)
+
